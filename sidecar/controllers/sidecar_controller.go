bdc55cb5da3300d36cb5caaba70901249f5a42ca
hakella10 <hakella10@gmail.com>
2021-06-03 13:38:41 +0000

    Added Reconcile Logic to update Pod and Sidecar

--- sidecar/controllers/sidecar_controller.go
+++ sidecar/controllers/sidecar_controller.go
@@ -18,12 +18,11 @@ package controllers
 
 import (
 	"context"
-        "fmt" 
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	corev1 "k8s.io/api/core/v1"
 	injectorv1alpha1 "argano.com/sidecar/api/v1alpha1"
@@ -34,6 +33,9 @@ type SidecarReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
+
+// Initialize logger
+var logger = logf.Log.WithName("sidecar-controller")
 
 //+kubebuilder:rbac:groups=injector.argano.com,resources=sidecars,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=injector.argano.com,resources=sidecars/status,verbs=get;update;patch
@@ -49,43 +51,74 @@ type SidecarReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *SidecarReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	_ = logf.FromContext(ctx)
 
+	logger = logf.Log.WithName(req.Namespace+"/"+req.Name)
 	// Add Controller Logic Here
-	// 1. Read the CRD Object of Sidecar
-	// 2. Get List of Active Pods with matching labels
 	// 3. if Pod has annotation not morked, then update the pod with sidecar container and mark the annotation
 	// 4. if Pod has annotation marked, then add the podNames to Sidecar.Status.Nodes
-	sidecarList := &injectorv1alpha1.SidecarList{}
-	err :=  r.List(ctx,sidecarList)
+
+	// 1. Get CRD Object of the Sidecar
+	sidecar := &injectorv1alpha1.Sidecar{}
+	err := r.Get(ctx,req.NamespacedName,sidecar)
 	if(err != nil){
-          fmt.Println("!!! Error while listing sidecars",err)
-	  return ctrl.Result{},err
+          logger.Error(err,"Unable to fetch Sidecar object ",req.Namespace,req.Name)
 	}
 
-	for i:=0;i<len(sidecarList.Items);i++ {
-	  podList := &corev1.PodList{}
-          err := r.List(ctx,podList,client.MatchingLabels(sidecarList.Items[i].Labels))
-	  if(err != nil) {
-            fmt.Println("!!! Error while listing Pods",err)
-	  }
+	// 2. Get List of Active Pods with matching labels
+	podList := &corev1.PodList{}
+	opts := []client.ListOption{
+	  client.InNamespace(req.NamespacedName.Namespace),
+	  client.MatchingLabels(sidecar.Labels),
+	}
 
-	  var podNames []string
-	  for j:=0;j<len(podList.Items);j++ {
-	    podNames = append(podNames,podList.Items[j].Name)
-	    pod   := podList.Items[j].DeepCopy()
-	    patch := client.MergeFrom(pod)
-	    err := r.Patch(ctx,pod,patch)
-	    if(err != nil){
-	      fmt.Println("!!! Unable to patch Pod",err)
+	err = r.List(ctx,podList,opts...)
+        if(err != nil){
+	  logger.Error(err,"Unable to list Pods",req.Namespace,req.Name)
+	}
+
+	// 3. If Pod has annotation not marked, then update the pod with sidecar container and mark the annotation
+	for i:=0;i<len(podList.Items);i++ {
+          pod := podList.Items[i]
+	  if pod.Status.Phase == "Running" {
+	    newPod := pod.DeepCopy()
+	    annotations :=  newPod.ObjectMeta.Annotations
+	    if(annotations == nil) {
+	      annotations = make(map[string]string)
 	    }
-	  }
+	    annotations["injector-sidecar"] = req.Name
+	    newPod.ObjectMeta.Annotations = annotations
+	    err := r.Update(ctx,newPod)
+	    if(err != nil){
+              logger.Error(err,"Unable to set annotations on Pod "+newPod.Name)
+	    }
+          } 
+	}
 
-	  // CRUD Sidecar Object using API methods defined here
-	  // Refer https://sdk.operatorframework.io/docs/building-operators/golang/references/client/
-	  sidecarList.Items[i].Status.Nodes = podNames
-	  sidecar := sidecarList.Items[i].DeepCopy()
-	  r.Status().Update(ctx,sidecar)
+	// 4. Get List of Pods and check if annotation is marked, then add the Pod name to Sidecar.Status.Nodes
+	podList = &corev1.PodList{}
+        err = r.List(ctx,podList,opts...)
+	if(err != nil){
+	  logger.Error(err,"Unable to list Pods",req.Namespace,req.Name)
+	}
+
+	var podNames []string
+	for i:=0;i<len(podList.Items);i++ {
+	  pod := podList.Items[i]
+	  if(pod.Status.Phase == "Running" && pod.Annotations["injector-sidecar"] == req.Name) {
+	    podNames = append(podNames,pod.Name)
+	  }
+	}
+
+        if(len(podNames) == 0) {
+          sidecar.Status.Nodes = make([]string,0,0)
+	} else {
+          sidecar.Status.Nodes = podNames
+	}
+
+	err = r.Status().Update(ctx,sidecar)
+	if(err != nil){
+          logger.Error(err,"Unable to update Sidecar.Status.Nodes")
 	}
 
 	return ctrl.Result{}, nil
