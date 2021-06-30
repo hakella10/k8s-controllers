fde0a70c3c99c647c288480f5e48fc782acff38d
hakella10 <hakella10@gmail.com>
2021-06-30 14:48:56 +0000

    add patchOperation

--- sidecar/webhook/webhook.go
+++ sidecar/webhook/webhook.go
@@ -7,7 +7,7 @@ import (
   "io/ioutil"
   "time"
   "encoding/json"
-  
+
   corev1 "k8s.io/api/core/v1"
   "k8s.io/api/admission/v1beta1"
   "k8s.io/apimachinery/pkg/runtime"
@@ -20,17 +20,25 @@ var (
   deserializer  = codecs.UniversalDeserializer()
 )
 
+type patchOperation struct {
+  Op    string `json:"op"`
+  Path  string `json:"path"`
+  Value interface{} `json:"value,omitempty"`
+}
+
 func init() {
   log.Println("You are init()")
 }
 
 func validate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
+  log.Println(ar)
+
   if(ar.Request == nil) {
     return &v1beta1.AdmissionResponse{
       Allowed: false,
     }
   }
-  
+
   var pod corev1.Pod
   req := ar.Request
   if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
@@ -39,27 +47,27 @@ func validate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
       Allowed: false,
     }
   }
-  
+
   return &v1beta1.AdmissionResponse{
     Allowed: true,
   }
 }
 
 func handleValidate(w http.ResponseWriter, r *http.Request) {
-  
+
   body, err := ioutil.ReadAll(r.Body)
   defer r.Body.Close()
   if err != nil {
     log.Println(err)
     http.Error(w, fmt.Sprintf("could not read body: %v", err), http.StatusInternalServerError)
   }
-  
+
   admissionReview  := v1beta1.AdmissionReview{}
   if _, _, err := deserializer.Decode(body, nil, &admissionReview); err != nil {
     log.Println(err)
     http.Error(w, fmt.Sprintf("could not decode body: %v", err), http.StatusInternalServerError)
   }
-  
+
   admissionResponse := mutate(&admissionReview)
   review := v1beta1.AdmissionReview{}
   if(admissionResponse != nil){
@@ -68,7 +76,7 @@ func handleValidate(w http.ResponseWriter, r *http.Request) {
         review.Response.UID = admissionReview.Request.UID
     }
   }
-  
+
   body, err = json.Marshal(review)
   if err != nil {
     log.Println(err)
@@ -76,17 +84,19 @@ func handleValidate(w http.ResponseWriter, r *http.Request) {
   }else {
     w.WriteHeader(http.StatusOK)
   }
-  
+
   w.Write(body)
 }
 
 func mutate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
+  log.Println(ar)
+
   if(ar.Request == nil) {
     return &v1beta1.AdmissionResponse{
       Allowed: false,
     }
   }
-  
+
   var pod corev1.Pod
   req := ar.Request
   if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
@@ -95,16 +105,17 @@ func mutate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
       Allowed: false,
     }
   }
-  
-  /*
-  patchBytes,err := json.Marshal([]byte(""))
+
+  patches := []patchOperation{}
+  patches  = append(patches,patchOperation{Op: "add", Path: "/metadata/annotations", Value: map[string]string{"sidecar-injector":"enabled"}, })
+  patchBytes,err := json.Marshal(patches)
   if(err != nil) {
     log.Println(err)
     return &v1beta1.AdmissionResponse{
       Allowed: false,
     }
   }
-  
+
   return &v1beta1.AdmissionResponse{
     Allowed: true,
     Patch:   patchBytes,
@@ -113,28 +124,24 @@ func mutate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
       return &pt
     }(),
   }
-  */
-  
-  return &v1beta1.AdmissionResponse{
-    Allowed: true,
-  }
+
 }
 
 func handleMutate(w http.ResponseWriter, r *http.Request) {
-  
+
   body, err := ioutil.ReadAll(r.Body)
   defer r.Body.Close()
   if err != nil {
     log.Println(err)
     http.Error(w, fmt.Sprintf("could not read body: %v", err), http.StatusInternalServerError)
   }
-  
+
   admissionReview  := v1beta1.AdmissionReview{}
   if _, _, err := deserializer.Decode(body, nil, &admissionReview); err != nil {
     log.Println(err)
     http.Error(w, fmt.Sprintf("could not decode body: %v", err), http.StatusInternalServerError)
   }
-  
+
   admissionResponse := mutate(&admissionReview)
   review := v1beta1.AdmissionReview{}
   if(admissionResponse != nil){
@@ -143,7 +150,7 @@ func handleMutate(w http.ResponseWriter, r *http.Request) {
         review.Response.UID = admissionReview.Request.UID
     }
   }
-  
+
   body, err = json.Marshal(review)
   if err != nil {
     log.Println(err)
@@ -151,7 +158,7 @@ func handleMutate(w http.ResponseWriter, r *http.Request) {
   }else {
     w.WriteHeader(http.StatusOK)
   }
-  
+
   w.Write(body)
 }
 
@@ -159,7 +166,7 @@ func main() {
   mux := http.NewServeMux()
   mux.HandleFunc("/validate", handleValidate)
   mux.HandleFunc("/mutate", handleMutate)
-  
+
   srvr := &http.Server{
     Addr:           ":8443",
     Handler:        mux,
@@ -167,6 +174,6 @@ func main() {
     WriteTimeout:   10 * time.Second,
     MaxHeaderBytes: 1 << 20,
   }
-  
+
   log.Fatal(srvr.ListenAndServeTLS("./ssl/webhook.crt", "./ssl/webhook.key"))
 }
